580bef0f29b25c4fb2340a1834035754b7aec3b9
comrade77 <nimec77@gmail.com>
2021-02-26 10:40:56 +0700

    Computer problem series #1: Fill the Hard Disk Drive

---
+++ lib/learn/fill_the_hard_disk_drive.dart
@@ -0,0 +1,25 @@
+class FillTheHardDiskDrive {
+  int save(List<int> sizes, int hd) {
+    int sum = 0;
+    int result = 0;
+    final mapSizes = sizes.asMap();
+    for (final index in mapSizes.keys) {
+      sum += mapSizes[index];
+      if (sum > hd) {
+        return result;
+      }
+      result = index + 1;
+    }
+    return result;
+  }
+
+  int saveBest(List<int> sizes, int hd) {
+    int leftSize = hd;
+    for (int i = 0; i < sizes.length; leftSize -= sizes[i++]) {
+      if (sizes[i] > leftSize) {
+        return i;
+      }
+    }
+    return sizes.length;
+  }
+}
