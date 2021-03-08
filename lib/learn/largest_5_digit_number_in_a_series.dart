5024fb59ba2f32c3fe9a616353e2809888de49d1
comrade77 <nimec77@gmail.com>
2021-03-08 11:14:16 +0700

    Largest 5 digit number in a series

---
+++ lib/learn/largest_5_digit_number_in_a_series.dart
@@ -0,0 +1,13 @@
+class Largest5DigitNumberInASeries {
+  int solution(String digits) {
+    int max = int.parse(digits.substring(0, 5));
+    for (int i = 5; i <= digits.length - 5; i++) {
+      final digit = int.parse(digits.substring(i, i + 5));
+      if (max < digit) {
+        max = digit;
+      }
+    }
+
+    return max;
+  }
+}
