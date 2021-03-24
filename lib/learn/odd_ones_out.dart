3d4515538c88624a06fe1a28ad309c41c6252b4a
comrade77 <nimec77@gmail.com>
2021-03-24 11:02:11 +0700

    Odd Ones Out!

---
+++ lib/learn/odd_ones_out.dart
@@ -0,0 +1,18 @@
+class OddOnesOut {
+  List<int> oddOnesOut(List<int> nums) {
+    final List<int> result = List.from(nums);
+    final unique = result.toSet();
+
+    for (final num in unique) {
+      final odd = nums.where((element) => element == num).length.isOdd;
+      if (odd) {
+        result.removeWhere((element) => element == num);
+      }
+    }
+    return result;
+  }
+
+  List<int> oddOnesOutBest(List<int> nums) {
+    return nums.where((x) => nums.where((y) => y == x).length.isEven).toList();
+  }
+}
