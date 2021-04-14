eb7ba674eef70926443fd42af7a4217d4c38b3a8
comrade77 <nimec77@gmail.com>
2021-04-14 14:54:26 +0700

    Common Denominators улучшенный вариант

--- lib/learn/5/common_denominators.dart
+++ lib/learn/5/common_denominators.dart
@@ -1,53 +1,28 @@
 class CommonDenominators {
   String convertFrac(List<List<int>> lst) {
-    final List<List<int>> list = [];
-    final Map<int, List<int>> factors = {};
-    for (final rational in lst) {
-      int m = rational[0];
-      int n = rational[1];
-      final divisor = m.gcd(n);
-      m ~/= divisor;
-      n ~/= divisor;
-      list.add([m, n]);
-      if (!factors.containsKey(n)) {
-        factors[n] = factorization(n);
-      }
-    }
-    List<int> commonDivisor = <int>[];
-    for (final rational in list) {
-      final n = rational[1];
-      commonDivisor += difference(factors[n]!, commonDivisor);
-    }
-    // print(commonDivisor);
-    final commonFactor = commonDivisor.fold<int>(1, (previousValue, element) => previousValue * element);
+    final list = lst.map((e) => reduced(e)).toList();
+
+    final ns = list.map((e) => e[1]).toList();
+    final divisors = ns.fold<int>(ns[0], lcm);
     final result = StringBuffer();
     for (final rational in list) {
       final m = rational[0];
       final n = rational[1];
-      final divisor = difference(commonDivisor, factors[n]!);
-      final factor = divisor.fold<int>(1, (previousValue, element) => previousValue * element);
-      result.write('(${m * factor},$commonFactor)');
+      result.write('(${m * divisors ~/ n},$divisors)');
     }
+
     return result.toString();
   }
 
-  List<int> factorization(int value) {
-    final result = [1];
-    int n = value;
-    for (int i = 2; i <= n; i++) {
-      while (n % i == 0) {
-        result.add(i);
-        n ~/= i;
-      }
-    }
-    return result;
+
+  List<int> reduced(List<int> list) {
+    final m = list[0];
+    final n = list[1];
+    final divisor = m.gcd(n);
+
+    return [m ~/ divisor, n ~/ divisor];
   }
 
-  List<int> difference(List<int> a, List<int> b) {
-    final result = [...a];
-    for (final item in b) {
-      result.remove(item);
-    }
-    return result;
-  }
+
+  int lcm(int a, int b) => a * b ~/ a.gcd(b);
 }
