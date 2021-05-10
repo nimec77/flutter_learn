4512342f3bfc10e553eba77798c80d4e7f2aaf40
comrade77 <nimec77@gmail.com>
2021-05-10 13:42:25 +0700

    Disease Spread

---
+++ lib/learn/6/disease_spread.dart
@@ -0,0 +1,45 @@
+class DiseaseSpread {
+  int epidemic(int tm, int n, int s0, int i0, double b, double a) {
+    int result = 0;
+
+    final dt = tm / n;
+    double s = s0.toDouble();
+    double i = i0.toDouble();
+    double r = 0;
+    for (int j = 1; j <= n; j++) {
+      final prevS = s;
+      final prevI = i;
+      final prevR = r;
+      s = prevS - dt * b * prevS * prevI;
+      i = prevI + dt * (b * prevS * prevI - a * prevI);
+      r = prevR + dt * prevI * a;
+      if (i < prevI) {
+        break;
+      }
+      if (result < i) {
+        result = i.ceil();
+      }
+    }
+
+    return result;
+  }
+
+  int epidemicBest(int tm, int n, int s0, int i0, double b, double a) {
+    final dt = tm / n;
+    double s = s0.toDouble();
+    double i = i0.toDouble();
+    double r = 0;
+
+    for (int j = 1; j <= n; j++) {
+      final nextI = i + dt * (b * s * i - a * i);
+      s -= dt * b * s * i;
+      r += dt * i * a;
+      if (nextI < i) {
+        return i.ceil();
+      }
+      i = nextI;
+    }
+
+    return i.ceil();
+  }
+}
---
+++ test/learn/6/disease_spread.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_learn/learn/6/disease_spread.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  bool aux(int tm, int n, int s0, int i0, double b, double a, int exp) {
+    final diseaseSpread = DiseaseSpread();
+    final actual = diseaseSpread.epidemicBest(tm, n, s0, i0, b, a);
+    final r = (actual - exp).abs();
+    final inrange = r <= 1;
+    if (inrange == false) {
+      print('abs(actual - expected) must be <= 1 but was $r');
+    }
+    return inrange;
+  }
+  dotest(int tm, int n, int s0, int i0, double b, double a, int exp) =>
+      test('Testing for $tm $n $s0 $i0 $b $a', () =>
+          expect(aux(tm, n, s0, i0, b, a, exp), equals(true)));
+
+  group('fixed tests', () {
+    dotest(18, 432, 1004, 1, 0.00209, 0.51, 420);
+    dotest(12, 288, 1007, 2, 0.00206, 0.45, 461);
+    dotest(13, 312, 999, 1, 0.00221, 0.55, 409);
+
+  });
+}
