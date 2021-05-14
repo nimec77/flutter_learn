3137e8e5850381644d9095267ecf0b095e85f4ad
comrade77 <nimec77@gmail.com>
2021-05-14 15:55:00 +0700

    Maximum product of n array elements

---
+++ lib/learn/algorithms/maximum_product.dart
@@ -0,0 +1,48 @@
+class MaximumProduct {
+  int maxProduct(List<int> list, int l) {
+    if (list.isEmpty) {
+      return 0;
+    }
+
+    final positives = <int>[];
+    final negatives = <int>[];
+
+    for (final item in list) {
+      if (item == 0) {
+        continue;
+      }
+      if (item > 0) {
+        positives.add(item);
+      } else {
+        negatives.add(item);
+      }
+    }
+    positives.sort((a, b) => b.compareTo(a));
+    negatives.sort((a, b) => a.compareTo(b));
+    if (negatives.isEmpty) {
+      return positives.take(l).fold(1, (previousValue, element) => previousValue * element);
+    }
+    if (positives.isEmpty && l.isOdd) {
+      return negatives.reversed.take(l).fold(1, (previousValue, element) => previousValue * element);
+    }
+    final p = List.filled(list.length, 1);
+    final n = List.filled(list.length, 1);
+
+    for (int i = 1; i <= positives.length; i++) {
+      p[i] = positives[i - 1] * p[i - 1];
+    }
+    for (int i = 1; i <= negatives.length; i++) {
+      n[i] = negatives[i - 1] * n[i - 1];
+    }
+
+    int max = 0;
+    for (int i = 1; i <= l / 2; i++) {
+      final k = p[l - 2 * i] * n[2 * i];
+      if (max < k) {
+        max = k;
+      }
+    }
+
+    return max;
+  }
+}
---
+++ test/algorithms/maximum_product_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_learn/learn/algorithms/maximum_product.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final maximumProduct = MaximumProduct();
+  group('Test Maximum Product', () {
+    test ('should return 0', () {
+      expect(maximumProduct.maxProduct([], 1), 0);
+    });
+    test('should return 300', () {
+      expect(maximumProduct.maxProduct([-100, 2, -3], 2), 300);
+    });
+    test ('should return 4320', () {
+      expect(maximumProduct.maxProduct([-10, 9, -8, -7, -6, 2, 1], 4), 3360);
+    });
+    test('should return 15120', () {
+      expect(maximumProduct.maxProduct(List.generate(10, (index) => index), 5), 15120);
+    });
+  });
+}
