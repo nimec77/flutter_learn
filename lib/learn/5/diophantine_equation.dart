19486e311f90bd80a8d6eab21b9a24d7b6ba8dd7
comrade77 <nimec77@gmail.com>
2021-07-17 15:40:37 +0700

    Рефактор Diophantine Equation

--- lib/learn/5/diophantine_equation.dart
+++ lib/learn/5/diophantine_equation.dart
@@ -2,15 +2,23 @@ import 'dart:math' as math;
 
 class DiophantineEquation {
   List<List<int>> solEqua(int n) {
-    var result = <List<int>>[];
-    for (final pair in pairsOfDivisors(n)) {
-      if ((pair.last - pair.first) % 4 == 0) {
-        final y = (pair.last - pair.first) ~/ 4;
-        final x = pair.first + 2 * y;
-        result.add([x, y]);
-      }
-    }
-    return result;
+    // var result = <List<int>>[];
+    // for (final pair in pairsOfDivisors(n)) {
+    //   if ((pair.last - pair.first) % 4 != 0) {
+    //     continue;
+    //   }
+    //   final y = (pair.last - pair.first) ~/ 4;
+    //   final x = pair.first + 2 * y;
+    //   result.add([x, y]);
+    // }
+    // return result;
+
+    return pairsOfDivisors(n).where((pair) => (pair.last - pair.first) % 4 == 0).map((pair) {
+      final y = (pair.last - pair.first) ~/ 4;
+      final x = pair.first + 2 * y;
+
+      return [x, y];
+    }).toList();
   }
 
   List<List<int>> pairsOfDivisors(int n) {
