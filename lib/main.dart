ce5b9761f7f793b72dbe44cc76ef9c14bce72363
comrade77 <nimec77@gmail.com>
2021-08-09 21:07:07 +0700

    Visit Ancestor Element

--- lib/main.dart
+++ lib/main.dart
@@ -12,23 +12,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Learn',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Учим Dart'),
-        ),
-        body: SingleChildScrollView(
-          child: Wrap(
-            children: [
-              Text(
-                digitalPalindrome.palindrome('1178711', 1).toString(),
-                style: Theme.of(context).textTheme.headline6,
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Center(
+        child: SizedBox.fromSize(
+          size: const Size(100, 100),
+          child: SizedBox.shrink(
+            child: SizedBox.shrink(
+              child: SizedBox.shrink(
+                child: SizedBox.shrink(
+                  child: SizedBox.shrink(
+                    child: SizedBox.shrink(
+                      child: Container(
+                        child: SizedBox.shrink(
+                          child: SizedBox.shrink(
+                            child: Builder(
+                              builder: (context) {
+                                var counter = 0;
+
+                                context.visitAncestorElements((element) {
+                                  debugPrint(element.widget.runtimeType.toString());
+                                  if (element.widget is SizedBox) {
+                                    counter++;
+                                  }
+                                  return true;
+                                });
+
+                                debugPrint('counter:$counter');
+                                return ColoredBox(
+                                  color: counter > 5 ? const Color(0xff00ff00) : const Color(0xffff0000),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ],
+            ),
           ),
         ),
       ),
