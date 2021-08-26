11d9bd14acceb9dc6cfdaf64c16e755c7f82d4a8
comrade77 <nimec77@gmail.com>
2021-08-26 10:22:26 +0700

    Error correction #1 - Hamming Code

---
+++ lib/learn/6/error_correction_1.dart
@@ -0,0 +1,34 @@
+class ErrorCorrection1 {
+  String encode(String text) {
+    return text.runes
+        .map((e) => e.toRadixString(2).padLeft(8, '0'))
+        .join()
+        .split('')
+        .expand((element) => [element * 3])
+        .join('');
+  }
+
+  String decode(String bits) {
+    final triples = splitByLength(bits, 3);
+    final correctedBits = triples.map((e) => e.split('1').length > 2 ? '1' : '0').join();
+    final bytes = splitByLength(correctedBits, 8);
+    final runes = bytes.map((e) => int.parse(e, radix: 2));
+
+    return String.fromCharCodes(runes);
+  }
+
+  Iterable<String> splitByLength(String value, int length) {
+    final result = <String>[];
+    for (var i = 0; i < value.length; i += length) {
+      result.add(value.substring(i, i + length));
+    }
+
+    return result;
+  }
+
+  String decodeBest(String bits) {
+    return bits
+        .replaceAllMapped(RegExp('...'), (match) => [0, 1, 2, 4].contains(int.parse(match[0]!, radix: 2)) ? '0' : '1')
+        .replaceAllMapped(RegExp('.{8}'), (match) => String.fromCharCode(int.parse(match[0]!, radix: 2)));
+  }
+}
