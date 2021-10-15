4f61fe456b3f25611f047e552d53c9efca85df12
comrade77 <nimec77@gmail.com>
2021-10-15 21:21:21 +0700

    Убра очередь из queen_problem.dart

--- lib/learn/algorithms/queen_problem/queen_problem.dart
+++ lib/learn/algorithms/queen_problem/queen_problem.dart
@@ -1,4 +1,3 @@
-import 'dart:collection';
 
 import 'package:flutter_learn/learn/algorithms/queen_problem/chessboard.dart';
 import 'package:flutter_learn/learn/algorithms/queen_problem/position.dart';
@@ -6,36 +5,31 @@ import 'package:flutter_learn/learn/algorithms/queen_problem/position.dart';
 const maxQueens = 8;
 
 class QueenProblem {
-  late Queue<Position> queue;
   late Chessboard chessboard;
 
   List<Position> queenProblem(final int startX) {
-    queue = Queue<Position>();
     chessboard = Chessboard.empty();
 
     final startPosition = Position(startX, 0);
     chessboard.addQueen(startPosition);
-    queue.add(startPosition);
     if (!_placeQueen(1)) {
       throw StateError('Failed to place $maxQueens queens on the chessboard');
     }
 
-    return queue.toList();
+    return chessboard.positions;
   }
 
   bool _placeQueen(final int y) {
-    if (queue.length >= maxQueens) {
+    if (chessboard.positions.length >= maxQueens) {
       return true;
     }
     for (final position in chessboard.places(y)) {
       chessboard.addQueen(position);
-      queue.add(position);
       final result = _placeQueen(y + 1);
       if (result) {
         return true;
       }
       chessboard.removeQueen(position);
-      queue.removeLast();
     }
 
     return false;
