5ccb4633ac27501ca5950dbbe9aec57e12686448
comrade77 <nimec77@gmail.com>
2021-10-15 17:05:18 +0700

    Пределал chessboard.dart, убрал представление доски

--- lib/learn/algorithms/queen_problem/chessboard.dart
+++ lib/learn/algorithms/queen_problem/chessboard.dart
@@ -4,12 +4,11 @@ import 'package:flutter_learn/learn/algorithms/queen_problem/position.dart';
 @immutable
 class Chessboard {
   Chessboard.empty()
-      : board =
-            List.generate(kBoardSize, (_) => List.generate(kBoardSize, (_) => false, growable: false), growable: false),
-        filledPlacesX = Map.from(List.generate(kBoardSize, (_) => false).asMap());
+      : filledPlacesX = Map.from(List.generate(kBoardSize, (_) => false).asMap()),
+        positions = [];
 
-  final List<List<bool>> board;
   final Map<int, bool> filledPlacesX;
+  final List<Position> positions;
 
   Iterable<Position> places(final int y) sync* {
     for (final pair in filledPlacesX.entries.where((element) => !element.value)) {
@@ -25,40 +24,27 @@ class Chessboard {
   }
 
   void addQueen(final Position position) {
-    assert(!board[position.y][position.x], 'This place is already taken');
-    board[position.y][position.x] = true;
+    positions.add(position);
     filledPlacesX[position.x] = true;
   }
 
   void removeQueen(final Position position) {
-    assert(board[position.y][position.x], 'The is no Queen in this place');
-    board[position.y][position.x] = false;
+    positions.remove(position);
     filledPlacesX[position.x] = false;
   }
 
   bool hasQueenBottom(final Position position) {
-    var pos = position;
-    while ((pos = pos.bottomTopStep) != position) {
-      if (board[pos.y][pos.x]) {
-        return true;
-      }
-    }
-    return false;
+    final add = position.x + position.y;
+
+    return positions.any((pos) => (pos.x + pos.y) == add);
   }
 
   bool hasQueenTop(final Position position) {
-    var pos = position;
-    while ((pos = pos.topBottomStep) != position) {
-      if (board[pos.y][pos.x]) {
-        return true;
-      }
-    }
+    final sub = position.x - position.y;
 
-    return false;
+    return positions.any((pos) => (pos.x - pos.y) == sub);
   }
 
   @override
-  String toString() {
-    return board.toString();
-  }
+  String toString() => positions.toString();
 }
